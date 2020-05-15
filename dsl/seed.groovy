af2ab83ead9053fb701fb51d425381311acaf008
Carlos Tomás <fenomenostranger@gmail.com>
2020-05-15 17:24:58 +0200

    .

--- dsl/seed.groovy
+++ dsl/seed.groovy
@@ -23,6 +23,7 @@ def createTestJob(jobName, repoUrl) {
     multibranchPipelineJob(jobName) {
         branchSources {
             git {
+                id('987654321') // IMPORTANT: use a constant and unique identifier
                 remote(repoUrl)
                 includes('*')
             }
@@ -34,7 +35,7 @@ def createTestJob(jobName, repoUrl) {
 }
 
 def buildPipelineJobs() {
-    def repo = "https://github.com/kcrane3576/"
+    def repo = "https://github.com/husker-du/"
     def repoUrl = repo + jobName + ".git"
     def deployName = jobName + "_deploy"
     def testName = jobName + "_test"
--- vars/jenkinsJob.groovy
+++ vars/jenkinsJob.groovy
@@ -15,12 +15,12 @@ def call(){
 
 def packageArtifact(){
     stage("Package artifact") {
-        sh "mvn package"
+        sh "./mvnw package"
     }
 }
 
 def buildAndTest(){
     stage("Backend tests"){
-        sh "mvn test"
+        sh "./mvnw test"
     }
 }
